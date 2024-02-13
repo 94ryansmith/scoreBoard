fd2d1e82f89d17e87bc191bbcfb410be5e4c06cd
Ryan Smith <81137372+94ryansmith@users.noreply.github.com>
2024-02-13 00:42:58 -0500

    Basketball scoreboard
    A scoreboard made to track a basketball game

---
+++ index.js
@@ -0,0 +1,69 @@
+let zeroHome = document.getElementById("zero-home") 
+let baseHome = 0 
+
+function homeOne() {
+     baseHome += 1
+    zeroHome.textContent = baseHome
+}
+
+function homeTwo() {
+    baseHome += 2
+    zeroHome.textContent = baseHome
+}
+
+function homeThr() {
+    baseHome += 3
+    zeroHome.textContent = baseHome
+}
+
+function hoReset() {
+    baseHome = 0
+    zeroHome.textContent = baseHome
+}
+
+
+let zeroAway = document.getElementById("zero-away")
+let baseAway = 0
+
+function awayOne() {
+    baseAway += 1
+   zeroAway.textContent = baseAway
+}
+
+function awayTwo() {
+    baseAway += 2
+    zeroAway.textContent = baseAway
+}
+
+function awayThr() {
+    baseAway += 3
+    zeroAway.textContent = baseAway
+}
+
+function awReset() {
+    baseAway = 0
+    zeroAway.textContent = baseAway
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
